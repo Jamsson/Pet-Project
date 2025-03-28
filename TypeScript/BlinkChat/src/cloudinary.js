fcf7501aa33ea9cf41deb60525e18256a0ea8484
Jamsson <123484091+Jamsson@users.noreply.github.com>
2025-03-28 22:04:14 +0200

    Update cloudinary.js

--- TypeScript/BlinkChat/src/cloudinary.js
+++ TypeScript/BlinkChat/src/cloudinary.js
@@ -1,9 +1,9 @@
 import { Cloudinary } from "cloudinary-core";
 
 const cloudinary = new Cloudinary({
-    cloud_name: "dpbojl4sb",
-    api_key: "343471925269259",
-    api_secret: "ZMb0z7CKhi0gbfOo2HBC_-Ly668",
+    cloud_name: "",
+    api_key: "",
+    api_secret: "",
 });
 
 export default cloudinary;
